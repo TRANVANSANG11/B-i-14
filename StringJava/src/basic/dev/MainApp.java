1994fb8aaa258bb13ce51f8582c5f7e4a590f0e0
TRANVANSANG11 <153897127+TRANVANSANG11@users.noreply.github.com>
2023-12-15 16:21:35 +0700

    Add files via upload

---
+++ StringJava/src/basic/dev/MainApp.java
@@ -0,0 +1,25 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+		public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Nhập vào một chuỗi: ");
+		String input = sc.nextLine();
+		String reversedString = "";
+		
+		for (int i = input.length() - 1; i >= 0; i--) {
+			reversedString += input.charAt(i);
+        }
+		System.out.println("Chuỗi đảo ngược: " + reversedString);
+	}
+}
+		
+
+
+	
+
+
